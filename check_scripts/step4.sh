0bd78988b75ed5ac22943e4951a5ac1d2833f22f
Tiryoh <tiryoh@gmail.com>
2018-12-13 17:35:48 +0900

    Fix step4

--- check_scripts/step4.sh
+++ check_scripts/step4.sh
@@ -16,28 +16,23 @@ echo 0 | tee /dev/rtmotor_raw_* 1>/dev/null
 #65535
 DIFF_R=$(($(cat /dev/rtcounter_r0) - $ENC_R))
 DIFF_L=$(($(cat /dev/rtcounter_l0) - $ENC_L))
+if [[ $DIFF_R -gt -65535 ]] && [[ $DIFF_R -lt 0 ]]; then
+	DIFF_R=$(($DIFF_R + 65535))
+fi
 if [[ $DIFF_R -gt 400 ]] && [[ $DIFF_R -lt 800 ]]; then
 	echo "RIGHT OK"
 else
-	if [[ $DIFF_R -gt -65535 ]] && [[ $DIFF_R -lt 0 ]]; then
-		DIFF_R=$(($DIFF_R + 65535))
-		if [[ $DIFF_R -gt 400 ]] && [[ $DIFF_R -lt 800 ]]; then
-			echo "RIGHT OK"
-		fi
-	fi
 	echo "echo 400 > /dev/rtmotor_raw_r0"
 	echo "ERROR"
 	echo "debug messege: right diff" $DIFF_R
 fi
+
+if [[ $DIFF_L -gt -65535 ]] && [[ $DIFF_L -lt 0 ]]; then
+	DIFF_L=$(($DIFF_L + 65535))
+fi
 if [[ $DIFF_L -gt 400 ]] && [[ $DIFF_L -lt 800 ]]; then
 	echo "LEFT OK"
 else
-	if [[ $DIFF_L -gt -65535 ]] && [[ $DIFF_L -lt 0 ]]; then
-		DIFF_L=$(($DIFF_L + 65535))
-		if [[ $DIFF_L -gt 400 ]] && [[ $DIFF_L -lt 800 ]]; then
-			echo "LEFT OK"
-		fi
-	fi
 	echo "echo 400 > /dev/rtmotor_raw_l0"
 	echo "ERROR"
 	echo "debug messege: left diff" $DIFF_L
@@ -52,28 +47,22 @@ sleep 1.0
 echo 0 | tee /dev/rtmotor_raw_* 1>/dev/null
 DIFF_R=$(($(cat /dev/rtcounter_r0) - $ENC_R))
 DIFF_L=$(($(cat /dev/rtcounter_l0) - $ENC_L))
+if [[ $DIFF_R -lt 65535 ]] && [[ $DIFF_R -gt 0 ]]; then
+	DIFF_R=$(($DIFF_R - 65535))
+fi
 if [[ $DIFF_R -lt -400 ]] && [[ $DIFF_R -gt -800 ]]; then
 	echo "RIGHT OK"
 else
-	if [[ $DIFF_R -lt 65535 ]] && [[ $DIFF_R -gt 800 ]]; then
-		DIFF_R=$(($DIFF_R - 65535))
-		if [[ $DIFF_R -lt -400 ]] && [[ $DIFF_R -gt -800 ]]; then
-			echo "RIGHT OK"
-		fi
-	fi
 	echo "echo -400 > /dev/rtmotor_raw_r0"
 	echo "ERROR"
 	echo "debug messege: right diff" $DIFF_R
 fi
+if [[ $DIFF_L -lt 65535 ]] && [[ $DIFF_L -gt 0 ]]; then
+	DIFF_L=$(($DIFF_L - 65535))
+fi
 if [[ $DIFF_L -lt -400 ]] && [[ $DIFF_L -gt -800 ]]; then
 	echo "LEFT OK"
 else
-	if [[ $DIFF_L -lt 65535 ]] && [[ $DIFF_L -gt 800 ]]; then
-		DIFF_L=$(($DIFF_L - 65535))
-		if [[ $DIFF_L -lt -400 ]] && [[ $DIFF_L -gt -800 ]]; then
-			echo "LEFT OK"
-		fi
-	fi
 	echo "echo -400 > /dev/rtmotor_raw_l0"
 	echo "ERROR"
 	echo "debug messege: left diff" $DIFF_L
@@ -89,12 +78,18 @@ sleep 1.0
 echo 0 | tee /dev/rtmotor_raw_* 1>/dev/null
 DIFF_R=$(($(cat /dev/rtcounter_r0) - $ENC_R))
 DIFF_L=$(($(cat /dev/rtcounter_l0) - $ENC_L))
+if [[ $DIFF_R -gt -65535 ]] && [[ $DIFF_R -lt 0 ]]; then
+	DIFF_R=$(($DIFF_R + 65535))
+fi
 if [[ $DIFF_R -gt 400 ]] && [[ $DIFF_R -lt 800 ]]; then
 	echo "RIGHT OK"
 else
 	echo "ERROR"
 	echo "echo 400 > /dev/rtmotor_raw_r0"
 	echo "debug messege: right diff" $DIFF_R
+fi
+if [[ $DIFF_L -lt 65535 ]] && [[ $DIFF_L -gt 0 ]]; then
+	DIFF_L=$(($DIFF_L - 65535))
 fi
 if [[ $DIFF_L -lt -400 ]] && [[ $DIFF_L -gt -800 ]]; then
 	echo "LEFT OK"
@@ -114,12 +109,18 @@ sleep 1.0
 echo 0 | tee /dev/rtmotor_raw_* 1>/dev/null
 DIFF_R=$(($(cat /dev/rtcounter_r0) - $ENC_R))
 DIFF_L=$(($(cat /dev/rtcounter_l0) - $ENC_L))
+if [[ $DIFF_R -lt 65535 ]] && [[ $DIFF_R -gt 0 ]]; then
+	DIFF_R=$(($DIFF_R - 65535))
+fi
 if [[ $DIFF_R -lt -400 ]] && [[ $DIFF_R -gt -800 ]]; then
 	echo "RIGHT OK"
 else
 	echo "ERROR"
 	echo "echo -400 > /dev/rtmotor_raw_r0"
 	echo "debug messege: right diff" $DIFF_R
+fi
+if [[ $DIFF_L -gt -65535 ]] && [[ $DIFF_L -lt 0 ]]; then
+	DIFF_L=$(($DIFF_L + 65535))
 fi
 if [[ $DIFF_L -gt 400 ]] && [[ $DIFF_L -lt 800 ]]; then
 	echo "LEFT OK"
