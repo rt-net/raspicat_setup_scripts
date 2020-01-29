0ae749690dacf0c8d09fbdb342f418eddc455b83
RT Corp <shop@rt-net.jp>
2020-01-29 12:00:56 +0900

    Bump up to v0.0.2

--- check_scripts/step1.sh
+++ check_scripts/step1.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 set -eu
 
-echo "Raspberry Pi Catテストスクリプト step1 Version 0.0.1"
-echo "LED1〜4が点滅していれば問題ありません。Ctrl - Cで停止してください。"
+echo "Raspberry Pi Catテストスクリプト step1 Version 0.0.2"
+echo "LED0～3が順番に点滅していれば問題ありません。Ctrl - Cで停止してください。"
 while true; do
-	echo 1 | tee /dev/rtled? 1>/dev/null
-	sleep 0.3
-	echo 0 | tee /dev/rtled? 1>/dev/null
-	sleep 0.3
+	echo 1 | tee /dev/rtled0 1>/dev/null
+	sleep 0.1
+	echo 0 | tee /dev/rtled0 1>/dev/null
+	echo 1 | tee /dev/rtled1 1>/dev/null
+	sleep 0.1
+	echo 0 | tee /dev/rtled1 1>/dev/null
+	echo 1 | tee /dev/rtled2 1>/dev/null
+	sleep 0.1
+	echo 0 | tee /dev/rtled2 1>/dev/null
+	echo 1 | tee /dev/rtled3 1>/dev/null
+	sleep 0.1
+	echo 0 | tee /dev/rtled3 1>/dev/null
+	sleep 1
 done
